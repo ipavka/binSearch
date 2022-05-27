cdb2609c30313e31958c67929e2f3681918ad0b2
ipavka <72130001+ipavka@users.noreply.github.com>
2022-05-27 15:06:36 +0300

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,5 +1,5 @@
 let num_min = [1];
-let num_max = [100];
+let num_max = [500];
 let userTry = 1;
 let variant = Math.floor((num_min[num_min.length - 1] + num_max[num_max.length - 1]) / 2)
 let userVariant = document.querySelector('.userVar').innerHTML =
@@ -9,7 +9,7 @@ let userVariant = document.querySelector('.userVar').innerHTML =
 
 function seyHello(obj) {
         userTry ++;
-        if (userTry === 8) {
+        if (userTry === 9) {
                 document.querySelector('body').innerHTML =
                     `Я угалал! Твоё число <br> <h3>${variant}</h3><h3>Попытки закончились!!!</h3>
                        <button onclick="location.reload()">Начать заново?</button> `
